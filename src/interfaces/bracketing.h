1c6e6a5a45241bd9bd3a7b7a575a68b1314642a1
Abdelrahman Mostafa <abdomody35@gmail.com>
2024-03-19 16:48:03 +0200

    implemented the bracketing interface class

--- src/interfaces/bracketing.h
+++ src/interfaces/bracketing.h
@@ -3,8 +3,8 @@
 class bracketing
 {
     protected:
-    virtual double findRoots(const std::string& equation, double& lower, double& upper, const int& n) = 0;
-    virtual double findRoots(const std::string& equation, double& lower, double& upper, const double& err) = 0;
+    virtual double findRoots(const std::string& equation, double lower, double upper, const int& n) = 0;
+    virtual double findRoots(const std::string& equation, double lower, double upper, const double& err) = 0;
 
     public:
     virtual ~bracketing() {};
