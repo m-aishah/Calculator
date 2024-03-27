801356f3968c8b17780de3ccb9fa99873d0d5abe
Aishah <maishah2540@gmail.com>
2024-03-27 21:56:21 +0200

    Improved implementation of linear regression - added functions to calculate coefficient of determinate and standard error.

--- src/operations/least-squares-regression/linear-regression.h
+++ src/operations/least-squares-regression/linear-regression.h
@@ -32,4 +32,56 @@ public:
 
         return std::make_pair(a0, a1);
     }
+
+    /**
+     * calculateRSquared - Calculates the coefficient of determination (R^2).
+     * @xValues: Vector containing x coordinates.
+     * @yValues: Vector containing y coordinates.
+     *
+     * Returns: The coefficient of determinate (R^2).
+     */
+    double calculateRSquared(const std::vector<double> &xValues, const std::vector<double> yValues, double a0, double a1)
+    {
+        int n = xValues.size();
+
+        // Calculate necessary summations
+        double yMean = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            yMean += yValues[i];
+        }
+        yMean /= n;
+
+        double sr = 0, st = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            double yPredicted = a0 + a1 * xValues[i];
+            sr += (yValues[i] - yPredicted) * (yValues[i] - yPredicted);
+            st += (yValues[i] - yMean) * (yValues[i] - yMean);
+        }
+
+        return 1.0 - (sr / st);
+    }
+
+    /**
+     * calculateStandardError - Calculate the standard error of estimate (sy/yx)
+     * @xValues: Vector containing x coordinates.
+     * @yValues: Vector containing y coordinates.
+     *
+     * Returns: The standard error of estimate.
+     */
+    double calculateStandardError(const std::vector<double> &xValues, const std::vector<double> &yValues, double a0, double a1)
+    {
+        int n = xValues.size();
+
+        // Calculate necessary summations
+        double sr = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            double yPredicted = a0 + a1 * xValues[i];
+            sr += (yValues[i] - yPredicted) * (yValues[i] - yPredicted);
+        }
+
+        return std::sqrt(sr / (n - 2));
+    }
 };
