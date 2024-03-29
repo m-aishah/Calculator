490a754a046c173939eb56a835ec700e6ee9eafd
godfreydekew <dekewgodfrey@gmail.com>
2024-03-29 19:21:25 +0200

    Newton interpolation

--- src/operations/interpolation/linear-interpolation.h
+++ src/operations/interpolation/linear-interpolation.h
@@ -4,6 +4,7 @@ class linearInterpolation: public Interpolation
 {
     private:
         double b0,b1 ,b2 ;
+        double fx1x0, fx2x1, fx3x2, fx2x1x0, fx3x2x1, fx3x2x1x0;
     public:  
         /**
          * interpolationPolation - performs a linear interpolation and returns aproximate
@@ -26,6 +27,7 @@ class linearInterpolation: public Interpolation
             }
             
         }
+        
 
         /**
          * interpolationPolation - performs a linear interpolation and returns aproximate
@@ -36,7 +38,7 @@ class linearInterpolation: public Interpolation
          * @fx0: interpolation fx0 value 
          * @fx1: interpolation fx1 value
          * @fx2: interpolation fx2 value
-         * Return: interpolation value of x if xo substituted in the equation 
+         * Return: interpolation value of y if xo substituted in the equation 
          */
         double quadraticData(double x,double x0,double x1,double x2,double fx0,double fx1,double fx2) override
         {
@@ -50,13 +52,65 @@ class linearInterpolation: public Interpolation
             b2 = ((fx2 - fx1) / (x2 - x1) - b1) / (x2 - x0);
             std::cout <<"b2=\t"<< b2<< std::endl;
             return (b0 + b1 * (x - x0) + b2 * (x - x0) * (x - x1));
-    }
+        }
+        
+
+        /**
+         * newonData - calculates fx using Newton’s Interpolating Polynomials
+         * Return: estimate value of fx
+        */
+        double newtonData(double x, double x0, double x1, double x2, double x3,double fx0, double fx1, double fx2, double fx3)
+        {
+            if (x1 - x0 == 0.0)
+            {
+                throw std::invalid_argument("Devision by zero in (x1 - x0) !");
+            }
+            fx1x0 = (fx1 - fx0) / (x1 - x0);
+            std::cout<< "fx1x0:"<< fx1x0 << std::endl;
+
+            if (x2 - x1 == 0.0)
+            {
+                throw std::invalid_argument("Devision by zero in (x2 - x1) !");
+            }
+            fx2x1 = (fx2 - fx1) / (x2 - x1);
+            std::cout<< "fx2x1:"<< fx2x1 << std::endl;
+
+            if (x3 - x2 == 0.0)
+            {
+                throw std::invalid_argument("Devision by zero in (x3 - x2) !");
+            }
+            fx3x2 = (fx3 - fx2) / (x3 - x2);
+            std::cout<< "fx3x2:"<< fx3x2 << std::endl;
+
+            if (x2 - x0 == 0.0)
+            {
+                throw std::invalid_argument("Devision by zero in (x2 - x0) !");
+            }
+            fx2x1x0 = (fx2x1 - fx1x0) / (x2 - x0);
+            std::cout<< "fx2x1x0:"<< fx2x1x0 << std::endl;
+
+            if (x3 - x1 == 0.0)  
+            {
+                throw std::invalid_argument("Devision by zero in (x3 - x1) !");
+            }
+            fx3x2x1 = (fx3x2 - fx2x1) / (x3 - x1);
+            std::cout<< "fx3x2x1:"<< fx3x2x1 << std::endl;
+
+             if (x3 - x0 == 0.0)
+             {
+                 throw std::invalid_argument("Devision by zero in (x3 - x0) !");
+             }
+            fx3x2x1x0 = (fx3x2x1 - fx2x1x0) / (x3 - x0);
+            std::cout<< "fx3x2x1x0:"<< fx3x2x1x0 << std::endl;
     
-    void reset() noexcept
-    {
-        b0 = 0.0;
-        b1 = 0.0;
-        b2 = 0.0;
-    }
+
+            return (fx0 + fx1x0 * (x - x0) + fx2x1x0 * (x - x0) * (x - x1) + fx3x2x1x0 * (x - x0) * (x - x1) * (x - x2) );
+        }
+        void reset() noexcept
+        {
+            b0 = 0.0;
+            b1 = 0.0;
+            b2 = 0.0;
+        }
 };
 
--- src/operations/interpolation/test1.cpp
+++ src/operations/interpolation/test1.cpp
@@ -1,27 +1,30 @@
 
 //include "quadratic-interpolation.h"
 #include "linear-interpolation.h"
+//#include "newton-interpolation.h"
 #include <iomanip>
 int main()
 {
-    linearInterpolation test1;
-    linearInterpolation test2;
+    //linearInterpolation test1;
+    linearInterpolation test3;
     //QuadraticInterpolation test2;
     //QuadraticInterpolation test2;
-    double fxo = 0.0, fx1 = 1.386294,x = 2.0,x1 = 4.0,x0 = 1.0,fx2 = 1.791759,x2 = 6.0;
+    double fxo = 0.0, fx1 = 1.386294,x = 2.0,x1 = 4.0,x0 = 1.0,fx2 = 1.791759,x2 = 6.0, x3 = 5, fx3 = 1.609438;
 
     std::cout << "-----------Welcome to the world of linear interpolation-------" << std::endl;
     /*Estimate the natural logarithm of 2 using linear interpolation. 
     First, perform the computation by interpolating between ln 1 5 0 and ln 6 5 1.791759. 
     Then, repeat the procedure, but use a smaller interval from ln 1 to ln 4 (1.386294). Note 
     that the true value of ln 2 is 0.6931472*/
-    auto fx = test1.linearData(x, x0, x1, fxo, fx1);
-    std::cout <<std::setprecision(9)<< "-----------first interval-------" << fx <<std::endl;
+    //auto fx = test1.linearData(x, x0, x1, fxo, fx1);
+    //std::cout <<std::setprecision(9)<< "-----------first interval-------" << fx <<std::endl;
     // second interval to be implemented
     
-    auto y = test2.quadraticData(x, x0, x1,x2 ,fxo, fx1,fx2);
-    std::cout << "Using quadratic interpolation " << y << std::endl;
+    //auto y = test2.quadraticData(x, x0, x1,x2 ,fxo, fx1,fx2);
+    //std::cout << "Using quadratic interpolation " << y << std::endl;*/
 
-
-
+    //using newton linear interpolation
+    auto y = test3.newtonData(x, x0, x1,x2 ,x3, fxo, fx1,fx2, fx3);
+    std::cout << "Using Newton interpolation " << y << std::endl;
+    return 0;
 }
